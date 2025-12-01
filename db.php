8d1ada4b546d555ed6352ea3cf3bd01c52b49014
atsumu-portfolio <nna487577@gmail.com>
2025-12-01 12:21:24 +0900

    Create db.php

---
+++ db.php
@@ -0,0 +1,18 @@
+<?php
+// db.php（ミニムサーバー用）
+
+$DB_HOST = 'mysql.coroproject.minim.ne.jp';
+$DB_NAME = 'db_coroproject_1';
+$DB_USER = 'db_coroproject';
+$DB_PASS = '4SLawVX2';
+
+$dsn = "mysql:host={$DB_HOST};dbname={$DB_NAME};charset=utf8mb4";
+
+try {
+    $pdo = new PDO($dsn, $DB_USER, $DB_PASS, [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ]);
+} catch (PDOException $e) {
+    exit('DB接続エラー: ' . $e->getMessage());
+}
