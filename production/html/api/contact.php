b52c2ab630a52171e1eb4a7e14efcdfaf07d4468
a.sadakata <nna487577@gmail.com>
2026-04-28 09:04:06 +0900

    フォルダ削除

--- production/html/api/contact.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-require_once dirname(__DIR__, 3) . '/includes/contact_submission.php';
-
-contact_handle_request([
-    'default_return_to' => '../thanks.html',
-    'default_source' => 'production',
-]);
