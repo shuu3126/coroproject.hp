4e93e4ebcd953cd45a9a50bef19ad4a47c955330
a.sadakata <nna487577@gmail.com>
2026-06-01 14:13:37 +0900

    Remove temporary test files

--- tamiya-home/check.php
+++
@@ -1,4 +0,0 @@
-<?php
-// 確認後すぐ削除してください
-echo 'CORO_DB_PASS: ' . (getenv('CORO_DB_PASS') !== false ? '設定あり（' . strlen(getenv('CORO_DB_PASS')) . '文字）' : '未設定') . '<br>';
-echo 'HTTP_HOST: ' . ($_SERVER['HTTP_HOST'] ?? '空') . '<br>';
