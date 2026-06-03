c16ecf673ad3f583771d959669a8f25c43a313f9
atsumu-portfolio <nna487577@gmail.com>
2026-06-03 13:46:37 +0900

    Remove tamiya-home from repo tracking

--- tamiya-home/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/includes/auth.php';
-
-if (isLoggedIn()) {
-    header('Location: /tamiya-home/dashboard.php');
-} else {
-    header('Location: /tamiya-home/login.php');
-}
-exit;
--- tamiya-home/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/includes/auth.php';
-session_destroy();
-header('Location: /tamiya-home/login.php');
-exit;
